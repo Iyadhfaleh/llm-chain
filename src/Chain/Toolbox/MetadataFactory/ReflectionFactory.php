35750f9975d9b3e4f0f3ab31b8a2691e7c2b2299
Iyadh Faleh <iyadh.faleh@gmail.com>
2025-03-29 17:55:19 +0100

    Fix remove unused imports

--- src/Chain/Toolbox/MetadataFactory/ReflectionFactory.php
+++ src/Chain/Toolbox/MetadataFactory/ReflectionFactory.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace PhpLlm\LlmChain\Chain\Toolbox\MetadataFactory;
 
-use PhpLlm\LlmChain\Chain\JsonSchema\Factory;
 use PhpLlm\LlmChain\Chain\Toolbox\Attribute\AsTool;
 use PhpLlm\LlmChain\Chain\Toolbox\Exception\ToolMetadataException;
-use PhpLlm\LlmChain\Chain\Toolbox\Metadata;
 
 /**
  * Metadata factory that uses reflection in combination with `#[AsTool]` attribute to extract metadata from tools.
